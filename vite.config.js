9594436d1252db922f5055f97869bf25d7291fd7
TonjeSchjefstad <tonsch03841@stud.noroff.no>
2025-09-22 14:02:21 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  appType: "mpa",
+  appType: "spa",
   base: "",
   build: {
     target: "esnext",
