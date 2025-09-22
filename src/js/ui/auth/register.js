e1d0ceec855ecf651cb3bc08d07c98d5c7539e48
TonjeSchjefstad <tonsch03841@stud.noroff.no>
2025-09-22 13:39:50 +0200

    Update register.js

---
+++ src/js/ui/auth/register.js
@@ -0,0 +1,21 @@
+import { registerUser } from "../../api/auth/register.js";
+import { navigate } from "../../router/index.js";
+import { showMessage } from "../../utilities/showMessage.js";
+
+export async function onRegisterFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const formFields = Object.fromEntries(formData);
+
+  try {
+    await registerUser(formFields);
+    showMessage("Registration successful! Redirecting to login...");
+    setTimeout(() => {
+      navigate("/auth/login/");
+    }, 1000);
+  } catch (error) {
+    console.error("register failed", error);
+    showMessage("Registration failed. Please try again.");
+  }
+}
