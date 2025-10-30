822c08670abf87c5b8cdb60f0bad42d4c94087b3
TonjeSchjefstad <tonsch03841@stud.noroff.no>
2025-10-30 16:36:09 +0100

    Add Tailwind v4 setup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   appType: "spa",
   base: "",
+  plugins: [tailwindcss()],
   build: {
     target: "esnext",
     rollupOptions: {
